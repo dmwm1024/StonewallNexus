b8fb30bd722d7e748006ac658d1ffa5526f6c030
D2Boomer <156481146+D2Boomer@users.noreply.github.com>
2025-07-08 18:11:11 -0400

    Update github_hooks.py

--- app/admin/routes/github_hooks.py
+++ app/admin/routes/github_hooks.py
@@ -10,12 +10,20 @@ GITHUB_SECRET = os.environ.get("GITHUB_SECRET", 'FAIL').encode()
 
 @admin.route('/github-webhook', methods=['POST'])
 def github_webhook():
-    signature = request.headers.get('X-Hub-Signature-256', '')
-    sha_name, signature = signature.split('=')
+    signature = request.headers.get("X-Hub-Signature-256")
+    if signature is None:
+        abort(401, "No signature provided.")
+
+    try:
+        sha_name, signature = signature.split('=')
+        if sha_name != 'sha256':
+            abort(401, "Unsupported signature method.")
+    except Exception:
+        abort(401, "Invalid signature format.")
+
     mac = hmac.new(GITHUB_SECRET, msg=request.data, digestmod=hashlib.sha256)
-
     if not hmac.compare_digest(mac.hexdigest(), signature):
-        abort(403)
+        abort(401, "Invalid signature.")
 
-    subprocess.Popen(["/home/kade/StonewallNexus/deploy_hook.py"])
-    return 'Success', 200
+    # Do your deployment work here
+    return "Success", 200
